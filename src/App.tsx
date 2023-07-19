fcf5dae0c0b468111ea90276c22f5803ed2d7d5d
khakaa <phr0210@naver.com>
2023-07-19 21:12:17 +0900

    [Feat] : App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-  return <div className='App'>test</div>;
-}
+import React from 'react';
+import { GlobalStyles } from './styles/GlobalStyles';
+import SearchBanner from './pages/SearchBanner';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <SearchBanner />
+    </>
+  );
+};
 
 export default App;
